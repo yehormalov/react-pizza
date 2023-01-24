998a544d1a899cd78e290f89cb742faa9bd60e38
Yehor <malovanyiyehor@icloud.com>
2023-01-24 10:11:30 +0100

    adaptive

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const stock = [
   {img: './img/pizza/8.png', title: 'Жюльен', price: 325, popularity: 9, filters: ['Вегетарианские']},
   {img: './img/pizza/9.png', title: '4 сезона', price: 500, popularity: 2, filters: ['Мясные', 'Гриль', 'Острые']},
   {img: './img/pizza/10.png', title: 'Сырная с ветчиной', price: 325, popularity: 8, filters: ['Мясные']},
-  {img: './img/pizza/11.png', title: 'Пепперони по-деревенски', price: 550, popularity: 13, filters: ['Мясные']},
+  {img: './img/pizza/11.png', title: 'По-деревенски', price: 550, popularity: 13, filters: ['Мясные']},
   {img: './img/pizza/12.png', title: '4 Сыра', price: 425, popularity: 14, filters: ['Вегетарианские', 'Гриль']},
   {img: './img/pizza/13.png', title: 'Веджи Fit & Fresh', price: 575, popularity: 11, filters: ['Вегетарианские']},
   {img: './img/pizza/14.png', title: 'Чикен BBQ', price: 360, popularity: 15, filters: ['Мясные', 'Гриль', 'Острые']},
